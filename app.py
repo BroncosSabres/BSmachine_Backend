c682fa381ddc35e88b7dd7b58346da2290d4fe94
BroncosSabres <broncossabres@gmail.com>
2025-04-24 23:43:25 +1000

    Update to not use local server

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
+import os
 
 app = Flask(__name__)
 
@@ -32,4 +33,5 @@ def latest_results():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
